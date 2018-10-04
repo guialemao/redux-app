89823addc7923c26cdfbd141aacd5e63ae82a367
Guilherme Milfont Moreno <guilherme.milfont@gmail.com>
2018-10-04 17:47:38 -0300

    added redux folders and connect the app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    const { newValue } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type='text' />
+          <button>
+            Click me!
+          </button>
+        <h1>{newValue}</h1>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = store => ({
+  newValue: store.clickReducer.newValue,
+});
+
+export default connect(mapStateToProps)(App);
